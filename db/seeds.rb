d1477963952f8a325b076a8f30df5f7f8a674346
Samir70 <sbetmouni@talktalk.net>
2023-07-26 15:45:12 +0100

    add seeds -- succesfully!

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,26 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-PractiseStyle.destroy_all
-dailyIncStyle = PractiseStyle.create(name: "daily increment", description: "The tempo to practise at increases by a set amount after each success.")
-fixedTempoStyle = PractiseStyle.create(name: "fixed tempo", description: "Practise at the same tempo every time.")
+Style.destroy_all
+dailyIncStyle = Style.create(name: "daily increment", description: "The tempo to practise at increases by a set amount after each success.")
+fixedTempoStyle = Style.create(name: "fixed tempo", description: "Practise at the same tempo every time.")
 
 puts("Seeded practise styles", dailyIncStyle, dailyIncStyle.name, dailyIncStyle.id)
 
-
 # had to make a table by hand otherwise get exception: 
-# no such table: main.practiseStyles
-# https://stackoverflow.com/questions/963420/undo-scaffolding-in-rails
-# https://stackoverflow.com/questions/330339/re-generating-scaffold
-# https://stackoverflow.com/questions/28535969/what-is-the-correct-way-to-generate-multi-word-reference-fields-in-rails
-# https://stackoverflow.com/questions/54160521/how-do-i-create-a-seed-file-using-a-model-with-references-im-not-sure-i-set-up
-Study.destroy_all
-sirDuke = Study.create!(title: "Sir Duke", content: "B major pentatonic riff", targetTempo: 110, curTempo: 95, backingTrack: "16th Note Funk", practiseStyle: fixedTempoStyle)
-hysteria = Study.create!(title: "Hysteria", content: "16th note riff", targetTempo: 95, curTempo: 90, backingTrack: "One rest rest rest", practiseStyle: dailyIncStyle)
+# no such table: main.PractiseStyles 
+# So redid the scaffold without camelCase
+# > rails db:rollback
+# > rails destroy scaffold PractiseStyle
+# > rails destroy scaffold Study
 
-puts("Seeded studies", sirDuke, sirDuke.id, sirDuke.practiseStyle_id)
+# Then remade scaffolds without camelCase
+# > rails generate scaffold Style name:string description:text 
+# > rails generate scaffold Study title:string content:text target_tempo:integer cur_tempo:integer backing_track:string style:references
+# > rails db:seed
+
+Study.destroy_all
+sirDuke = Study.create!(title: "Sir Duke", content: "B major pentatonic riff", target_tempo: 110, cur_tempo: 95, backing_track: "16th Note Funk", style: fixedTempoStyle)
+hysteria = Study.create!(title: "Hysteria", content: "16th note riff", target_tempo: 95, cur_tempo: 90, backing_track: "One rest rest rest", style: dailyIncStyle)
+
+puts("Seeded studies", sirDuke, sirDuke.id, sirDuke.style)
